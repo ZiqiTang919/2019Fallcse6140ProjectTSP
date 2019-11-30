47fa86ef992c12a25587b46c77dd6691b83a6e7f
ZiqiTang919 <32851589+ZiqiTang919@users.noreply.github.com>
2019-11-30 18:23:27 -0500

    add functions for output

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 
 def read_data(filename):
-	file = open(filename)
-	for _ in range(5):
+	file = open(filename, 'r')
+	line = file.readline()
+	name = line.split(' ')[-1][:-1]
+	for _ in range(4):
 		file.readline()
 	
 	X = []
@@ -19,30 +21,59 @@ def read_data(filename):
 		y = float(line[2])
 		X.append([x, y])
 
-	return squareform(pdist(X, 'euclidean'))
+	return name, squareform(pdist(X, 'euclidean'))
+
+def write_solution(filename, sol, route):
+	file = open(filename, 'w')
+	file.write(str(sol) + '\n')
+	file.write(','.join([str(v) for v in route])) 
+
+def write_trace(filename, trace):
+	file = open(filename, 'w')
+	for row in trace:
+		file.write(str(row[0]) + ',' + str(row[1]) + '\n') 
+
+def BNB(distance_matrix, time):
+	return None, None, None
+def MST(distance_matrix, time):
+	return None, None, None
+def SA(distance_matrix, time, seed):
+	return None, None, None
+def ILS(distance_matrix, time, seed):
+	return None, None, None
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-inst", dest="filename", required=True)
-parser.add_argument("-alg", dest="algorithm", required=True)
-parser.add_argument("-time", dest="time", required=True)
-parser.add_argument("-seed", dest="seed")
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-inst", dest="filename", required=True)
+	parser.add_argument("-alg", dest="algorithm", required=True)
+	parser.add_argument("-time", dest="time", required=True)
+	parser.add_argument("-seed", dest="seed")
 
-args = parser.parse_args()
+	args = parser.parse_args()
 
-distance_matrix = read_data(args.filename)
+	name, distance_matrix = read_data(args.filename)
+	outname = '{}_{}_{}'.format(name, args.algorithm, args.time)
 
-if args.algorithm == 'BnB':
-	best_sol, best_route, trace = BNB(distance_matrix, args.time)
-elif args.algorithm == 'Approx':
-	best_sol, best_route, trace = MST(distance_matrix, args.time)
-elif args.algorithm == 'LS1':
-	best_sol, best_route, trace = SA(distance_matrix, args.time, args.seed)
-elif args.algorithm == 'LS2':
-	best_sol, best_route, trace = ILS(distance_matrix, args.time, args.seed)
-else:
-	sys.exit('wrong format!')
+	if args.algorithm == 'BnB':
+		best_sol, best_route, trace = BNB(distance_matrix, args.time)
+	elif args.algorithm == 'Approx':
+		best_sol, best_route, trace = MST(distance_matrix, args.time)
+	elif args.algorithm == 'LS1':
+		best_sol, best_route, trace = SA(distance_matrix, args.time, args.seed)
+		outname += '_{}'.format(args.seed)
+	elif args.algorithm == 'LS2':
+		best_sol, best_route, trace = ILS(distance_matrix, args.time, args.seed)
+		outname += '_{}'.format(args.seed)
+	else:
+		sys.exit('wrong format!')
 
+	best_sol = 1000
+	best_route = [1,2,3,4,5]
+	trace = [[10, 1111], [50, 1100], [100, 1000]]
+
+	write_solution(outname+'.sol', best_sol, best_route)
+	write_trace(outname+'.trace', trace)
 
 
 
